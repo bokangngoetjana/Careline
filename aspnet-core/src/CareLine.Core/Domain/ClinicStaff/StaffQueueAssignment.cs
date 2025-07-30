56e1377e3910802de238336e9577924538cb384d
Bokang <bokangngoetjana06@gmail.com>
2025-07-31 00:33:49 +0200

    chore:  Added the rest of the entities

---
+++ aspnet-core/src/CareLine.Core/Domain/ClinicStaff/StaffQueueAssignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+using CareLine.Domain.Queues;
+
+namespace CareLine.Domain.ClinicStaff
+{
+    public class StaffQueueAssignment : FullAuditedEntity<Guid>
+    {
+        public Guid StaffId { get; set; }
+        public Guid QueueId { get; set; }
+
+        public Staff Staff { get; set; }
+        public Queue Queue { get; set; }
+        public DateTime AssignedDate { get; set; } 
+    }
+}
---
+++ aspnet-core/src/CareLine.Core/Domain/Tickets/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+
+namespace CareLine.Domain.Tickets
+{
+    public class Ticket : FullAuditedEntity<Guid>
+    {
+        public Guid PatientId { get; set; }
+        public Guid? StaffId { get; set; }
+        public Guid QueueId { get; set; }
+        public Guid ServiceTypeId { get; set; }
+
+        public string Symptoms { get; set; }
+        public int QueueNumber { get; set; }
+
+    }
+}
